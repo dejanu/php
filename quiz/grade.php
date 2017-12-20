858b6ce66d9cb423ede8819807a44fd1304e6ff5
Dej <dejanualex@yahoo.com>
2017-12-20 21:00:24 +0200

    Update group

--- quiz/grade.php
+++ quiz/grade.php
@@ -1,20 +1,14 @@
 <?php
 $name = strip_tags($_POST['x']);
 $surname = strip_tags($_POST['x']);
-
 echo "Hello: ".$name."<hr>";
-
 if (isset($name) && isset($surname)){
-
-      if (isset($_POST["unu"])){
-    echo "Correct : ".$_POST["unu"];
+      if ($_POST["test"] == "one"){
+    echo "Correct : ".$_POST["test"];
         }
       else {
           echo "Incorrect ";
           die();
           }
 }
-
-
-
  ?>
