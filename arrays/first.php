d27459a407f6ad08913d471b9c49fc8b99a0d9a3
Dej <dejanualex@yahoo.com>
2017-12-20 15:36:24 +0200

    array iteration

--- arrays/first.php
+++ arrays/first.php
@@ -3,7 +3,7 @@ $names = array('Alex','Billy','Vitor');
 //numeric array = list
 
 //echo $names will generate error
-echo '<pre>',print_r ($names);
+//echo '<pre>',print_r ($names);
 
 //associative array = dict
 $person = array(
@@ -14,4 +14,38 @@ $person = array(
 
 print_r($person,true);
 
+//foreach only used for associative Array
+// foreach($person as $key => $value){
+//   echo $value;
+// }
+
+
+//Calculate max value from an arrays
+$numbers = array("one"=>1, "two"=>2, "three"=>33, "four"=>4);
+
+function max_array($nums){
+  $local_max = 0;
+  foreach ($nums as $k=>$v){
+    if ($v > $local_max){
+      $local_max =  $v;
+    }
+  }
+  return $local_max;
+
+//bubble sort
+  $x = array (83, 32, 51, 12, 8, 9, 1);
+
+  function sorter ($arr){
+      for ($i=0;$i<=count($arr)-1;$i++){
+        for ($j=0;$j<=count($arr)-1;$j++){
+            if ($arr[$i]>$arr[$j]) {
+               $aux = $arr[$i];
+               $arr[$i] = $arr[$j];
+               $arr[$j] = $aux;
+            }
+        }
+      }
+      return $arr;
+  }
+}
  ?>
---
+++ arrays/nested_array_iteration.php
@@ -0,0 +1,27 @@
+<?php
+//associative Array
+
+$modules = array("os" => array("path" => 1,
+                              "chdir" => 2,
+                              "listdir" => 3),
+                "sys" => array( "exit"=> 5,
+                               "platform"=> 6,
+                               "version" => 7),
+                "random"=>"randrange",
+                  );
+
+//var_dump($modules);
+//echo('<hr />');
+
+//array iteration
+// foreach ($modules as $k =>$v) {
+//       //array to string notice due to the fact that we have nested arrays
+//       foreach ($v as $key => $value) {
+//         echo $k." : ".$key." : ".$value."<br>";
+//       }
+// }
+
+
+
+
+ ?>
